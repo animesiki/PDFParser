4c0e57928d58e6c4058e2a61f03d0d3580d082c8
William Jiang <william.jiang@oocl.com>
2015-12-09 21:34:15 +0800

    format code

--- src/main/java/com/oocl/frm/pdf/parser/XMLOutputParser.java
+++ src/main/java/com/oocl/frm/pdf/parser/XMLOutputParser.java
@@ -1,21 +1,20 @@
 package com.oocl.frm.pdf.parser;
 
+import com.snowtide.PDF;
+import com.snowtide.pdf.Document;
+import pdfts.examples.XMLOutputTarget;
+
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
-import pdfts.examples.XMLOutputTarget;
-
-import com.snowtide.PDF;
-import com.snowtide.pdf.Document;
-
 public class XMLOutputParser implements IPdfParser {
 
-	public String parsePDF(byte[] pdfContent) throws IOException {
-		Document pdf = PDF.open(new ByteArrayInputStream(pdfContent), null);
-		XMLOutputTarget xmlOutputTarget = new XMLOutputTarget();
-		pdf.pipe(xmlOutputTarget);
-		pdf.close();
-		return xmlOutputTarget.getXMLAsString();
-	}
+    public String parsePDF(byte[] pdfContent) throws IOException {
+        Document pdf = PDF.open(new ByteArrayInputStream(pdfContent), null);
+        XMLOutputTarget xmlOutputTarget = new XMLOutputTarget();
+        pdf.pipe(xmlOutputTarget);
+        pdf.close();
+        return xmlOutputTarget.getXMLAsString();
+    }
 
 }
